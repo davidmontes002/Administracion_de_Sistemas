0087c3911cf2231f5cb4c9989b2a08b861ccb9f3
davidmontes002 <VaddiTesmon0.2@outlook.com>
2026-03-23 16:19:26 -0700

    Parche del parche

--- Windows_Server_2/Active_Directory/parche_f4.ps1
+++ Windows_Server_2/Active_Directory/parche_f4.ps1
@@ -1,4 +1,5 @@
 $Dominio = (Get-ADDomain).DistinguishedName
+$NombreDominio = (Get-ADDomain).Name
 $GpoAppLocker = "GPO_AppLocker"
 
 Write-Host "1. Creando Grupo de Seguridad y agregando a 'no cuates'..." -ForegroundColor Cyan
@@ -10,18 +11,15 @@ $UsuariosNoCuates = Get-ADUser -Filter * -SearchBase "OU=no_cuates,$Dominio"
 foreach ($User in $UsuariosNoCuates) {
     Add-ADGroupMember -Identity "Grupo_NoCuates" -Members $User -ErrorAction SilentlyContinue
 }
-$GrupoSID = (Get-ADGroup "Grupo_NoCuates").SID.Value
 
 Write-Host "2. Preparando GPO limpia..." -ForegroundColor Cyan
 Remove-GPO -Name $GpoAppLocker -ErrorAction SilentlyContinue
 $Gpo = New-GPO -Name $GpoAppLocker
 New-GPLink -Name $GpoAppLocker -Target $Dominio | Out-Null
-
-# ¡LA CORRECCIÓN ESTÁ AQUÍ! Las llaves {} alrededor del ID
 $RutaLdap = "LDAP://CN={$($Gpo.Id)},CN=Policies,CN=System,$Dominio"
 
-Write-Host "3. Construyendo la arquitectura XML de AppLocker..." -ForegroundColor Cyan
-$xml = @"
+Write-Host "3. Inyectando Salvavidas (Path) mediante XML puro..." -ForegroundColor Cyan
+$xmlSalvavidas = @"
 <AppLockerPolicy Version="1">
   <RuleCollection Type="Exe" EnforcementMode="Enabled">
     <FilePathRule Id="921cc481-6e17-4653-8f75-050b80acca20" Name="(Salvavidas) Program Files" Description="" UserOrGroupSid="S-1-1-0" Action="Allow">
@@ -33,24 +31,26 @@ $xml = @"
     <FilePathRule Id="fd686d83-a829-4351-8ff4-27c7de5755d2" Name="(Salvavidas) Administradores" Description="" UserOrGroupSid="S-1-5-32-544" Action="Allow">
       <Conditions><FilePathCondition Path="*" /></Conditions>
     </FilePathRule>
-    <FilePathRule Id="d84793f7-53c7-4abf-b88a-36fb222a7288" Name="Bloqueo Notepad System32" Description="" UserOrGroupSid="$GrupoSID" Action="Deny">
-      <Conditions><FilePathCondition Path="%SYSTEM32%\notepad.exe" /></Conditions>
-    </FilePathRule>
-    <FilePathRule Id="e84793f7-53c7-4abf-b88a-36fb222a7289" Name="Bloqueo Notepad Windows" Description="" UserOrGroupSid="$GrupoSID" Action="Deny">
-      <Conditions><FilePathCondition Path="%WINDIR%\notepad.exe" /></Conditions>
-    </FilePathRule>
   </RuleCollection>
 </AppLockerPolicy>
 "@
+$TempXmlSalvavidas = "$env:TEMP\Salvavidas.xml"
+$xmlSalvavidas | Out-File $TempXmlSalvavidas -Encoding UTF8
+Set-AppLockerPolicy -XmlPolicy $TempXmlSalvavidas -Ldap $RutaLdap
 
-$TempXmlPath = "$env:TEMP\AppLockerPolicy.xml"
-$xml | Out-File $TempXmlPath -Encoding UTF8
+Write-Host "4. Calculando HASH criptográfico de Notepad e inyectando..." -ForegroundColor Cyan
+$NotepadInfo = Get-AppLockerFileInformation -Path "C:\Windows\System32\notepad.exe"
+$XmlHash = New-AppLockerPolicy -RuleType Hash -User "$NombreDominio\Grupo_NoCuates" -FileInformation $NotepadInfo -Xml
+# Invertir el permiso de Permitir a Denegar
+$XmlHash = $XmlHash -replace 'Action="Allow"', 'Action="Deny"'
+$TempXmlHash = "$env:TEMP\NotepadHash.xml"
+$XmlHash | Out-File $TempXmlHash -Encoding UTF8
 
-Write-Host "4. Inyectando XML en la Directiva de Grupo (GPO)..." -ForegroundColor Cyan
-Set-AppLockerPolicy -XmlPolicy $TempXmlPath -Ldap $RutaLdap
+# El parámetro -Merge une la regla Hash con el XML de Salvavidas que pusimos arriba
+Set-AppLockerPolicy -XmlPolicy $TempXmlHash -Ldap $RutaLdap -Merge
 
 Write-Host "5. Configurando auto-arranque del servicio en clientes..." -ForegroundColor Cyan
 $AppIdKey = "HKLM\System\CurrentControlSet\Services\AppIDSvc"
 Set-GPRegistryValue -Name $GpoAppLocker -Key $AppIdKey -ValueName "Start" -Type DWord -Value 2 | Out-Null
 
-Write-Host "`n¡Éxito total! GPO de AppLocker aplicada sin errores." -ForegroundColor Green
+Write-Host "`n¡Éxito total! GPO aplicada y Notepad bloqueado por HASH." -ForegroundColor Green
